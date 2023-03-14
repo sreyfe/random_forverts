eaacd8a12082aec5d527cbafafa0b63e31212ecd
Jonah Lubin <127442578+sreyfe@users.noreply.github.com>
2023-03-14 12:05:56 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ import tweepy
 from PIL import Image, ImageChops
 
 
-auth = tweepy.OAuthHandler("PJnh83QnsZoFrPY2I3Yc8s3iz", "Lk2YiVhkBZMyY1PW8aM8E9A6Tp9NDHZsYV9LXhlLuMIWaJaPIa")
-auth.set_access_token("1471244735536939014-EGPHlYwCxs8Z6eh0TlwD97iO6YCEXO", "Z0dPZTURFCd06AU2pnl7WvEaNQPIJKNGCB5DKAHpFXmWk")
+auth = tweepy.OAuthHandler("")
+auth.set_access_token("")
 api = tweepy.API(auth, )
 media_ids = []
 art_url = ""
